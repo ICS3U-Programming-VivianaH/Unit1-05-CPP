60d6f5c8321263c5832f49807f6b77183c604e95
Viviana Hurtado <viviana.hurtado-antiporta@stu.ocsb.ca>
2025-02-19 02:33:58 +0000

    Added areaPerRect.cpp

---
+++ areaPerRect.cpp
@@ -0,0 +1,15 @@
+//  Copyright 2025 Viviana Hurtado
+//  Name: Viviana Hurtado
+//  Date: 18/02/25
+//  Course: ICS3U Computer Science
+//  Description: this program calculates the area and perimeter of a rectangle with dimensions 5cm x 3cm
+
+#include <iostream>
+
+int main() {
+    std::cout << "If a rectangle has the dimensions" << std::endl;
+    std::cout << "5cm x 3cm" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "Area is:" << (5*3) << "cm^2" << std::endl;
+    std::cout << "Perimeter is:" << (4 + 5 * 2) << std::endl;
+}
